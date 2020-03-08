94b6d4b36db567dcade9a296b6c30cee39a03e37
fredjust <fredjust@gmail.com>
2020-03-08 14:38:29 +0100

    premier envoi
    tous les fichiers

---
+++ OAuth2/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMakeBotMove = New System.Windows.Forms.Button()
+        Me.txtMove = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtToken = New System.Windows.Forms.TextBox()
+        Me.btnGetGames = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtGameId = New System.Windows.Forms.TextBox()
+        Me.txtGames = New System.Windows.Forms.TextBox()
+        Me.btnMakeBoardMove = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnMakeBotMove
+        '
+        Me.btnMakeBotMove.Location = New System.Drawing.Point(182, 178)
+        Me.btnMakeBotMove.Name = "btnMakeBotMove"
+        Me.btnMakeBotMove.Size = New System.Drawing.Size(108, 23)
+        Me.btnMakeBotMove.TabIndex = 0
+        Me.btnMakeBotMove.Text = "Make a Bot Move"
+        Me.btnMakeBotMove.UseVisualStyleBackColor = True
+        '
+        'txtMove
+        '
+        Me.txtMove.Location = New System.Drawing.Point(76, 180)
+        Me.txtMove.Name = "txtMove"
+        Me.txtMove.Size = New System.Drawing.Size(100, 20)
+        Me.txtMove.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Token"
+        '
+        'txtToken
+        '
+        Me.txtToken.Location = New System.Drawing.Point(56, 36)
+        Me.txtToken.Name = "txtToken"
+        Me.txtToken.Size = New System.Drawing.Size(161, 20)
+        Me.txtToken.TabIndex = 3
+        '
+        'btnGetGames
+        '
+        Me.btnGetGames.Location = New System.Drawing.Point(223, 34)
+        Me.btnGetGames.Name = "btnGetGames"
+        Me.btnGetGames.Size = New System.Drawing.Size(75, 23)
+        Me.btnGetGames.TabIndex = 4
+        Me.btnGetGames.Text = "Get Games"
+        Me.btnGetGames.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(301, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Game Id :"
+        '
+        'txtGameId
+        '
+        Me.txtGameId.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtGameId.Location = New System.Drawing.Point(360, 59)
+        Me.txtGameId.Name = "txtGameId"
+        Me.txtGameId.Size = New System.Drawing.Size(100, 13)
+        Me.txtGameId.TabIndex = 6
+        '
+        'txtGames
+        '
+        Me.txtGames.Location = New System.Drawing.Point(304, 36)
+        Me.txtGames.Name = "txtGames"
+        Me.txtGames.Size = New System.Drawing.Size(608, 20)
+        Me.txtGames.TabIndex = 7
+        '
+        'btnMakeBoardMove
+        '
+        Me.btnMakeBoardMove.Location = New System.Drawing.Point(182, 207)
+        Me.btnMakeBoardMove.Name = "btnMakeBoardMove"
+        Me.btnMakeBoardMove.Size = New System.Drawing.Size(108, 23)
+        Me.btnMakeBoardMove.TabIndex = 8
+        Me.btnMakeBoardMove.Text = "BOARD Move"
+        Me.btnMakeBoardMove.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(942, 408)
+        Me.Controls.Add(Me.btnMakeBoardMove)
+        Me.Controls.Add(Me.txtGames)
+        Me.Controls.Add(Me.txtGameId)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnGetGames)
+        Me.Controls.Add(Me.txtToken)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtMove)
+        Me.Controls.Add(Me.btnMakeBotMove)
+        Me.Name = "Form1"
+        Me.Text = "API Lichess TEST"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnMakeBotMove As Button
+    Friend WithEvents txtMove As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtToken As TextBox
+    Friend WithEvents btnGetGames As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtGameId As TextBox
+    Friend WithEvents txtGames As TextBox
+    Friend WithEvents btnMakeBoardMove As Button
+End Class
---
+++ OAuth2/Form1.vb
@@ -0,0 +1,158 @@
+﻿'Imports System
+'Imports System.Threading.Tasks
+Imports System.Text
+Imports System.Net
+Imports System.IO
+'Imports Newtonsoft.Json
+
+
+
+
+Public Class Form1
+
+    Const Token As String = "PAST TOKEN HERE"
+
+    'Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    '    Dim origResponse As HttpWebResponse
+
+
+    '    Dim origRequest As HttpWebRequest = DirectCast(HttpWebRequest.Create("https://lichess.org/api/account/playing"), HttpWebRequest)
+    '    origRequest.Headers.Add("Authorization", "Bearer TOKEN")
+    '    origRequest.AllowAutoRedirect = False
+    '    origRequest.Method = "GET"
+    '    Try
+    '        origResponse = DirectCast(origRequest.GetResponse(), HttpWebResponse)
+    '        Dim Stream As Stream = origResponse.GetResponseStream()
+    '        Dim sr As New StreamReader(Stream, Encoding.GetEncoding("utf-8"))
+    '        Dim str As String = sr.ReadToEnd()
+    '        MessageBox.Show(str)
+    '        Clipboard.SetText(str)
+    '    Catch ex As WebException
+    '        MsgBox(ex.ToString)
+
+    '    End Try
+    'End Sub
+
+
+
+    Private Function MakeBotMove(idGame As String, uciMove As String, token As String) As Boolean
+        Dim origResponse As HttpWebResponse
+
+
+        Dim origRequest As HttpWebRequest = DirectCast(HttpWebRequest.Create("https://lichess.org/api/bot/game/" & idGame & "/move/" & uciMove), HttpWebRequest)
+        origRequest.Headers.Add("Authorization", "Bearer " & token)
+        origRequest.AllowAutoRedirect = False
+        origRequest.Method = "POST"
+        Try
+            origResponse = DirectCast(origRequest.GetResponse(), HttpWebResponse)
+            Dim Stream As Stream = origResponse.GetResponseStream()
+            Dim sr As New StreamReader(Stream, Encoding.GetEncoding("utf-8"))
+            Dim str As String = sr.ReadToEnd()
+            Return True
+            'MessageBox.Show(str)
+            'Clipboard.SetText(str)
+        Catch ex As WebException
+
+            'MsgBox(ex.ToString)
+            Return False
+        End Try
+    End Function
+
+    Private Function MakeBoardMove(idGame As String, uciMove As String, token As String) As Boolean
+        Dim origResponse As HttpWebResponse
+
+
+        Dim origRequest As HttpWebRequest = DirectCast(HttpWebRequest.Create("https://lichess.org/api/board/game/" & idGame & "/move/" & uciMove), HttpWebRequest)
+        origRequest.Headers.Add("Authorization", "Bearer " & token)
+        origRequest.AllowAutoRedirect = False
+        origRequest.Method = "POST"
+        Try
+            origResponse = DirectCast(origRequest.GetResponse(), HttpWebResponse)
+            Dim Stream As Stream = origResponse.GetResponseStream()
+            Dim sr As New StreamReader(Stream, Encoding.GetEncoding("utf-8"))
+            Dim str As String = sr.ReadToEnd()
+            Return True
+            'MessageBox.Show(str)
+            'Clipboard.SetText(str)
+        Catch ex As WebException
+
+            'MsgBox(ex.ToString)
+            Return False
+        End Try
+    End Function
+
+    Private Function GetOngoingGame(token As String) As String
+        Dim origResponse As HttpWebResponse
+
+
+        Dim origRequest As HttpWebRequest = DirectCast(HttpWebRequest.Create("https://lichess.org/api/account/playing"), HttpWebRequest)
+        origRequest.Headers.Add("Authorization", "Bearer " & token)
+        origRequest.AllowAutoRedirect = False
+        origRequest.Method = "GET"
+        Try
+            origResponse = DirectCast(origRequest.GetResponse(), HttpWebResponse)
+            Dim Stream As Stream = origResponse.GetResponseStream()
+            Dim sr As New StreamReader(Stream, Encoding.GetEncoding("utf-8"))
+            Dim str As String = sr.ReadToEnd()
+            Return str
+            'MessageBox.Show(str)
+            'Clipboard.SetText(str)
+        Catch ex As WebException
+
+            'MsgBox(ex.ToString)
+            Return ""
+        End Try
+    End Function
+
+    Private Sub BtnGetProfile_Click(sender As Object, e As EventArgs) Handles btnGetGames.Click
+        txtGames.Text = GetOngoingGame(txtToken.Text)
+        txtGameId.Text = GetJSONdata(txtGames.Text, "gameId")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtToken.Text = Token
+
+    End Sub
+
+
+
+    'renvoie la valeur d'une variable a partir de son nom dans une chaine JSON
+    Private Function GetJSONdata(ByVal strJSON As String, ByVal data_name As String, Optional ByVal RemoveCote As Boolean = True) As String
+        Dim idStart As Integer
+        Dim idEnd As Integer
+        Dim Str_tempo As String
+
+        If data_name = "" Then Return ""
+
+        idStart = strJSON.IndexOf("""" & data_name & """:")
+
+        If idStart = -1 Then Return ""
+
+        idStart = idStart + data_name.Length + 3
+
+        idEnd = strJSON.IndexOf(",", idStart)
+
+        'si c'est la dernière variable de la chaine il n'y a pas de , après mais une }
+        If idEnd = -1 Then
+            idEnd = strJSON.IndexOf("}", idStart)
+        End If
+
+        Str_tempo = strJSON.Substring(idStart, idEnd - idStart)
+
+        'on supprime les doubles cotes
+        If RemoveCote And Str_tempo.IndexOf("""") = 0 Then
+            Str_tempo = Str_tempo.Substring(1, Str_tempo.Length - 2)
+        End If
+
+        Return Str_tempo
+
+    End Function
+
+    Private Sub BtnMakeBotMove_Click(sender As Object, e As EventArgs) Handles btnMakeBotMove.Click
+        MakeBotMove(txtGameId.Text, txtMove.Text, txtToken.Text)
+    End Sub
+
+    Private Sub BtnMakeBoardMove_Click(sender As Object, e As EventArgs) Handles btnMakeBoardMove.Click
+        MakeBoardMove(txtGameId.Text, txtMove.Text, txtToken.Text)
+    End Sub
+End Class
